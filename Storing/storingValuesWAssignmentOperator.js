41ef4247d50ab8fcddaba637097bec576c242f2a
balalis43 <balalis43@gmail.com>
2021-12-30 15:46:53 +0000

    1st Commit

---
+++ Storing/storingValuesWAssignmentOperator.js
@@ -0,0 +1,18 @@
+/* Data Types:
+undefined, null, boolean, string, symbol, number, and object
+*/
+
+var myName = "Stove"
+
+myName = 88
+
+let ourName = "freeCodeRamp"
+
+const pi = 3.14 var a;
+var b = 2;
+console.log(a)
+a = 7;
+
+b = a;
+
+console.log(a);
---
+++ index.js
@@ -0,0 +1,11 @@
+/* Data Types:
+undefined, null, boolean, string, symbol, number, and object
+*/
+
+var myName = "Stove"
+
+myName = 88
+
+let ourName = "freeCodeRamp"
+
+const pi = 3.14
